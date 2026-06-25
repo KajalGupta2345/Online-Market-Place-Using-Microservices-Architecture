7c992ea5fc6b96516812ac4e307ff0d928ee52c5
Kajal Gupta <kk2347994@gmail.com>
2026-06-25 15:48:46 +0530

    final

--- seller/src/broker/listeners.js
+++ seller/src/broker/listeners.js
@@ -1,47 +1,52 @@
 const userModel = require('../models/user.model');
 const orderModel = require('../models/order.models');
 const productModel = require('../models/product.model');
-const {subscribeToQueue} = require('./broker');
+const { subscribeToQueue } = require('./broker');
 const paymentModel = require('../models/payment.models');
 
 
-module.exports = function(){
-    subscribeToQueue('AUTH_SELLER_DASHBOARD.USER_CREATED', async(user) => {
+module.exports = function () {
+    subscribeToQueue('AUTH_SELLER_DASHBOARD.USER_CREATED', async (user) => {
+        const { _id, ...userData } = user;
         await userModel.findOneAndUpdate(
-            { username: user.username },
-            user,
+            { $or: [{ username: user.username }, { email: user.email }] },
+            { $set: userData },
             { upsert: true, new: true }
         );
     });
 
-    subscribeToQueue('PRODUCT_SELLER_DASHBOARD.PRODUCT_CREATED', async(product) => {
+    subscribeToQueue('PRODUCT_SELLER_DASHBOARD.PRODUCT_CREATED', async (product) => {
+        const { _id, ...productData } = product;
         await productModel.findOneAndUpdate(
             { _id: product._id },
-            product,
+            { $set: productData },
             { upsert: true, new: true }
         );
     });
 
-    subscribeToQueue('ORDER_SELLER_DASHBOARD.ORDER_CREATED', async(orders) => {
+    subscribeToQueue('ORDER_SELLER_DASHBOARD.ORDER_CREATED', async (orders) => {
+        const { _id, ...orderData } = orders;
         await orderModel.findOneAndUpdate(
             { _id: orders._id },
-            orders,
+            { $set: orderData },
             { upsert: true, new: true }
         );
     });
 
-    subscribeToQueue('PAYMENT_SELLER_DASHBOARD.PAYMENT.CREATED', async(payment) => {
+    subscribeToQueue('PAYMENT_SELLER_DASHBOARD.PAYMENT.CREATED', async (payment) => {
+        const { _id, ...paymentData } = payment;
         await paymentModel.findOneAndUpdate(
             { order: payment.order },
-            payment,
+            { $set: paymentData },
             { upsert: true, new: true }
         );
     });
 
-    subscribeToQueue('PAYMENT_SELLER_DASHBOARD.PAYMENT.UPDATED', async(payment) => {
+    subscribeToQueue('PAYMENT_SELLER_DASHBOARD.PAYMENT.UPDATED', async (payment) => {
+        const { _id, ...paymentData } = payment;
         await paymentModel.findOneAndUpdate(
             { order: payment.order },
-            { ...payment }
+            { $set: paymentData }
         );
     });
 }
