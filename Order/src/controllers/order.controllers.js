f57eb32773502c50554951143de5172c0309f175
Kajal Gupta <kk2347994@gmail.com>
2026-06-25 11:33:11 +0530

    final

--- Order/src/controllers/order.controllers.js
+++ Order/src/controllers/order.controllers.js
@@ -6,7 +6,7 @@ const { publishToQueue } = require('../broker/broker');
 
 async function createOrder(req, res) {
   const user = req.user;
-  const token = req.cookies?.token || req.headers?.Authorization?.split(' ')[1];
+  const token = req.cookies?.token || req.headers?.authorization?.split(' ')[1];
 
   try {
     const cartResponse = await axios.get(`${process.env.CART_SERVICE_URL}/api/cart/items`, {
