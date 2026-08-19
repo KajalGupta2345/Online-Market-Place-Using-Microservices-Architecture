5de48b891c382f331771fe99e902c6af0addd954
Kajal Gupta <kk2347994@gmail.com>
2026-08-19 12:57:55 +0530

    final

--- seller/src/app.js
+++ seller/src/app.js
@@ -1,6 +1,7 @@
 const cookieParser = require('cookie-parser');
 const express = require('express');
 const sellerRoutes = require('./routes/seller.routes');
+const cors = require('cors');
 
 const app = express();
 
